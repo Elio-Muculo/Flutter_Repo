6dbc155f9dff11c9d5677ac7ff39543aeba1632f
Elliot <emuculo25@gmail.com>
2022-07-11 20:07:56 +0200

    fix tabview problem

--- lib/Task UI/Componentes/Home.dart
+++ lib/Task UI/Componentes/Home.dart
@@ -39,7 +39,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
                     bottomRight: Radius.circular(30))),
             child: Tabs(),
           ),
-          
+
         ],
       ),
     );
@@ -93,10 +93,10 @@ class _TabsState extends State<Tabs> with TickerProviderStateMixin {
               child: Container(
                 decoration: BoxDecoration(
                   color: const Color(0xFF63d6d3),
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(6),
                 ),
                 child: TabBar(
-                  padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 4),
+                  padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 4),
                   unselectedLabelColor: Colors.white,
                   labelColor: const Color(0xFF33c8c6),
                   indicator: BoxDecoration(
@@ -126,21 +126,69 @@ class _TabsState extends State<Tabs> with TickerProviderStateMixin {
   }
 }
 
-class Freelancer extends StatelessWidget {
+
+class Freelancer extends StatefulWidget {
   const Freelancer({Key? key}) : super(key: key);
+
+  @override
+  State<Freelancer> createState() => _FreelancerState();
+}
+
+class _FreelancerState extends State<Freelancer> with TickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 4, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-              child: const Text(
-                'Freelancer',
-                // style: TextStyle(color: Color.fromARGB(0, 0, 0, 0)),
-            ),
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 4.h, vertical: 2.w),
+                child: TabBar(
+                  isScrollable: true,
+                  labelPadding: EdgeInsets.symmetric(horizontal: 5.w),
+                  indicatorSize: TabBarIndicatorSize.label,
+                  unselectedLabelColor: const Color.fromARGB(147, 255, 255, 255),
+                  labelColor: const Color.fromARGB(255, 255, 255, 255),
+                  indicatorColor: Colors.white,
+                  controller: tabController,
+                  tabs: const [
+                    Text('All'), 
+                    Text('Open'),
+                    Text('Ongoing'),
+                    Text('Done'),
+                  ]
+                ),
+              ),
+          ],
+        ),
+       Expanded(
+         child: TabBarView(
+            controller: tabController,
+            children: const [
+              Employeer(),
+              Employeer(),
+              Employeer(),
+              Employeer()
+            ]
           ),
-        ],
+       ),
+      ],
     );
   }
 }
