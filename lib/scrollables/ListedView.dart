385b2675cd0667d6f88c1d7386e48c1e5de66f45
Elliot <emuculo25@gmail.com>
2022-07-14 19:25:40 +0200

    scrollables lesson

---
+++ lib/scrollables/ListedView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class view extends StatelessWidget {
+  const view({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: 
+      ),
+    );
+  }
+}
