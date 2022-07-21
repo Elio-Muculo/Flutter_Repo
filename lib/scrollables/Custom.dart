afdc2963db14e7972b9f2036c78d6f25168a2151
Elliot <emuculo25@gmail.com>
2022-07-22 00:39:52 +0200

    custom scrooll view

---
+++ lib/scrollables/Custom.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Custom extends StatefulWidget {
+  Custom({Key? key}) : super(key: key);
+
+  @override
+  State<Custom> createState() => _CustomState();
+}
+
+class _CustomState extends State<Custom> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
